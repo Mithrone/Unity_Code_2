826ede45d818842d865a04584dcbb342170bd179
Zander van Tonder <zandermarko.vantonder@gmail.com>
2020-07-06 14:30:41 +0200

    fffffffffffffffffffff

---
+++ spriteMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spriteMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject a;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
